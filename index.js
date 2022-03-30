44590aac59675f851bcce9999696b1a5b9921627
Alex Salazar <alexwsalazar@gmail.com>
2022-03-30 01:27:44 -0500

    added questions to my index.js

--- index.js
+++ index.js
@@ -1,60 +1,108 @@
-// TODO: Include packages needed for this application
-const inquirer = require(inquirer);
-const fs = require('fs');
-const generateSampleHTML = require("./utils/generateSampleHTML")
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const inquirer = require("inquirer")
+const path = require("path")
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const output = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./utils/generateSampleHTML");
+
+// Empty array for team objects 
+const employeeArr = [];
+
+// Manager specific questions for inquier.prompt
+
+const managerRole = {
+  type: "input",
+  name: "officeNumber",
+  message: "what is their office number?",
+  validate: answer => {
+    if (answer.length< 1){
+      return "you need to enter an office number."
+    } return true;
+  }
+};
+
+// engineer specific questions for inquier.prompt
+
+const engineerRole = {
+  type: "input",
+  message: "what is their Github user name",
+  name: "github",
+  validate: answer => {
+    if (answer.length < 1){
+      return "you need to enter their username."
+    } return true;
+  }
+};
 
 
+// Intern specific questions for inquier.prompt
 
-function init(){
-inquirer.prompt({
-  type:"loop",
-  name: "items",
-  message: "add another employee?",
-  questions:[{      
-      type: 'input',
-      message: 'What is your name?',
-      name: 'name',
-    },
-    {
-      type: 'input',
-      message: 'what is your email?',
-      name: 'email',
-    },
-    {
-      type: 'input',
-      message: 'what is your id',
-      name: 'id',
-    },
-    {
-        type: 'input',
-        message: 'What is your office numner?',
-        name: 'office number',
-    },
-    {
-        type: 'input',
-        message: 'what is your github username?',
-        name: 'github',
-    },
-    {
-        type: 'input',
-        message: 'what school did you attend?',
-        name: 'school',
-      },
-  ]
-  .then((response) => {
+const internRole = {
+  type: "input",
+  name: "school",
+  message: "what school do they attend?",
+  validate: answer => {
+    if (answer.length < 1){
+      return "you need to enter a school."
+    } return true;
+  }
+};
 
-    fs.writeFileSync('Sample.html', generateSampleHTML(response))
-    
-  }),
-// }
-// function generateSampleHTML(data) {
-//   return `
+// General employee questions for inquier.prompt
 
-// `;
-},
+const employeeQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "what is the name of the employee?",
+    validate: answer => {
+      if (answer.length < 1){
+        return "you need to enter a name for the employee";
+      } return true;
+    }
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "what is their ID?",
+    validate: answer => {
+      if (answer.length < 1){
+        return "you need to enter an ID number for the employee";
+      } return true;
+    }
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "what is the email for this employee?",
+    validate: answer => {
+      if (answer.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
+        return true;
+      } else{
+        return "invalid email. Please enter valid email"
+      };
+    }
+  },
+  {
+    type: "rawlist",
+    name: "role",
+    message: "what is the role of the employee?",
+    choices: ["Manager", "Engineer", "Intern"]
+  }
+];
 
+// Question to add another employee
 
-module.exports = generateSampleHTML());
-// Function call to initialize app
-init();
-}
+const anotherEmployee = 
+{
+  type: "confirm",
+  name: "another",
+  message: "do you want to add another team member?"
+};
+
+// Init function to start inquirer.prompt
