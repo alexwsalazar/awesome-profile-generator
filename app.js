e5be2f95500d5d387076bfa5f29f19d4b51481bf
Alex Salazar <alexwsalazar@gmail.com>
2022-03-30 21:25:58 -0500

    deleted extra steps

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const inquirer = require("inquirer")
 const path = require("path")
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+
 
 const generate = require("./lib/generateHTML");
 
